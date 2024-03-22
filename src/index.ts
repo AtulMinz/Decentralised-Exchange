3b17171445c1e7566af9bb84fab05eae12d79fe5
AtulMinz <atulminz.jsr@gmail.com>
2024-03-23 01:55:39 +0530

    Initial Setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import express, { Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 
 const app: Express = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from bun");
+});
 
 app.listen(3000, () => {
-  console.log("up and runnig");
+  console.log("up and running at http://localhost:3000");
 });
